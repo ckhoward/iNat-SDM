0cdc589d673da298c1783bc4907f028146818932
A. Phillip Johnson <aphillipjohnson@email.arizona.edu>
2017-10-26 18:18:22 -0700

    CSV organizations
    Still need to remove non research grade observations, and format dates

--- organize.py
+++ organize.py
@@ -1,11 +1,33 @@
+import csv
 taxon_dict={}
 def get_organized(file):
 	file=open(file, 'r')
 	for line in file:
 		line=line.split()
 		taxon_dict[line[0]]=' '.join(line[1:])
-		print(taxon_dict[line[0]])
+		#print(taxon_dict[line[0]])
 	return taxon_dict
 		
 hello = get_organized('taxon_list.txt')
-print(taxon_dict)
+#print(taxon_dict)
+data_dict={}
+def get_data(filename):
+	with open(filename,  encoding='utf8') as csvfile:
+		reader = csv.DictReader(csvfile)
+		data_keys=data_dict.keys()
+		for row in reader:
+			if row['taxonID'] not in data_keys:
+				data_dict[row['taxonID']]= {}
+				data_dict[row['taxonID']]['date'] = [row['eventDate']] 
+				data_dict[row['taxonID']]['latitude'] = [row['decimalLatitude']]
+				data_dict[row['taxonID']]['longitude'] = [row['decimalLongitude']]
+			else:	
+				data_dict[row['taxonID']]['date'].append(row['eventDate'])
+				data_dict[row['taxonID']]['latitude'].append(row['decimalLatitude'])
+				data_dict[row['taxonID']]['longitude'].append(row['decimalLongitude'])
+			
+			#print(row['taxonID'])
+			
+			
+get_data('observations.csv')
+print(data_dict['27818'])
