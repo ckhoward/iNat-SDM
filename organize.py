b0b889703b1140c54f5bbb2736f16f7bee18078d
A. Phillip Johnson <aphillipjohnson@email.arizona.edu>
2017-10-29 17:49:06 -0700

    changed organizations scheme

--- organize.py
+++ organize.py
@@ -6,6 +6,7 @@ def get_organized(file):
 		line=line.split()
 		taxon_dict[line[0]]=' '.join(line[1:])
 		#print(taxon_dict[line[0]])
+	file.close()
 	return taxon_dict
 		
 hello = get_organized('taxon_list.txt')
@@ -17,17 +18,59 @@ def get_data(filename):
 		data_keys=data_dict.keys()
 		for row in reader:
 			if row['taxonID'] not in data_keys and row['datasetName'] == 'iNaturalist research-grade observations':
-				data_dict[row['taxonID']]= {}
-				data_dict[row['taxonID']]['date'] = [row['eventDate']] 
-				data_dict[row['taxonID']]['latitude'] = [row['decimalLatitude']]
-				data_dict[row['taxonID']]['longitude'] = [row['decimalLongitude']]
+				data_dict[row['taxonID']]= {}				
+				'''
+				organizing dictionary input into format {{year}, {month}, [lat, long]}
+				'''
+				if row['eventDate'][4]=='-':
+					data_dict[row['taxonID']][row['eventDate'][0:4]]={}
+					data_dict[row['taxonID']][row['eventDate'][0:4]]['01']=[]
+					data_dict[row['taxonID']][row['eventDate'][0:4]]['02']=[]
+					data_dict[row['taxonID']][row['eventDate'][0:4]]['03']=[]
+					data_dict[row['taxonID']][row['eventDate'][0:4]]['04']=[]
+					data_dict[row['taxonID']][row['eventDate'][0:4]]['05']=[]
+					data_dict[row['taxonID']][row['eventDate'][0:4]]['06']=[]
+					data_dict[row['taxonID']][row['eventDate'][0:4]]['07']=[]
+					data_dict[row['taxonID']][row['eventDate'][0:4]]['08']=[]
+					data_dict[row['taxonID']][row['eventDate'][0:4]]['09']=[]
+					data_dict[row['taxonID']][row['eventDate'][0:4]]['10']=[]
+					data_dict[row['taxonID']][row['eventDate'][0:4]]['11']=[]
+					data_dict[row['taxonID']][row['eventDate'][0:4]]['12']=[]
+					
+					
+					#format is Latitude, longitude
+					data_dict[row['taxonID']][row['eventDate'][0:4]][row['eventDate'][5:7]].append([row['decimalLatitude'],row['decimalLongitude']])
+					#print(data_dict[row['taxonID']][row['eventDate'][0:4]])
+					
+					
 			elif row['datasetName'] == 'iNaturalist research-grade observations':	
-				data_dict[row['taxonID']]['date'].append(row['eventDate'])
-				data_dict[row['taxonID']]['latitude'].append(row['decimalLatitude'])
-				data_dict[row['taxonID']]['longitude'].append(row['decimalLongitude'])
-			
-			#print(row['taxonID'])
-			
+				'''
+				This gets rid of all non research grade observations
+				'''	
+				if len(row['eventDate'])>0 and row['eventDate'][4]=='-':
+					year_key = data_dict[row['taxonID']].keys()
+					if row['eventDate'][0:4]not in year_key:
+						data_dict[row['taxonID']][row['eventDate'][0:4]]={}
+						data_dict[row['taxonID']][row['eventDate'][0:4]]['01']=[]
+						data_dict[row['taxonID']][row['eventDate'][0:4]]['02']=[]
+						data_dict[row['taxonID']][row['eventDate'][0:4]]['03']=[]
+						data_dict[row['taxonID']][row['eventDate'][0:4]]['04']=[]
+						data_dict[row['taxonID']][row['eventDate'][0:4]]['05']=[]
+						data_dict[row['taxonID']][row['eventDate'][0:4]]['06']=[]
+						data_dict[row['taxonID']][row['eventDate'][0:4]]['07']=[]
+						data_dict[row['taxonID']][row['eventDate'][0:4]]['08']=[]
+						data_dict[row['taxonID']][row['eventDate'][0:4]]['09']=[]
+						data_dict[row['taxonID']][row['eventDate'][0:4]]['10']=[]
+						data_dict[row['taxonID']][row['eventDate'][0:4]]['11']=[]
+						data_dict[row['taxonID']][row['eventDate'][0:4]]['12']=[]
+						data_dict[row['taxonID']][row['eventDate'][0:4]][row['eventDate'][5:7]].append([row['decimalLatitude'],row['decimalLongitude']])
+
+					else:
+						data_dict[row['taxonID']][row['eventDate'][0:4]][row['eventDate'][5:7]].append([row['decimalLatitude'],row['decimalLongitude']])
+				
+						#print(row['taxonID'])
+		
 			
 get_data('observations.csv')
 print(data_dict['27818'])
+				
